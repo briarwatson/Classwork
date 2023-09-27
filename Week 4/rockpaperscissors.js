ff1c26ee42a1949d911c694d4adabc071355fab1
briarwatson <briar.watson402@gmail.com>
2023-09-27 14:47:14 -0500

    new

--- Week 4/rockpaperscissors.js
+++ Week 4/rockpaperscissors.js
@@ -16,6 +16,34 @@ function computerPick() {
 function userPick(e) {
     const userChoice = e.target.value;
     userDisplay.innerText = `You chose $(userChoice)`;
+    computerPick();
+    userDisplay.innerText = `You chose ${userChoice}`;
+    if (compChoice === 0) {
+        compDisplay.innerText = "The computer chose rock";
+    } else if (compChoice === 1) {
+        compDisplay.innerText = "The computer chose paper";
+    } else if (compChoice === 2) {
+        compDisplay.innerText = "The computer chose scissors";
+    }
 
-
+    // See if the user won
+    if (userChoice === 'paper' && compChoice === 0 ||
+        userChoice === 'rock' && compChoice === 2 ||
+        userChoice === 'scissors' && compChoice === 1) {
+            endGame("win");
+    } else if (userChoice === 'paper' && compChoice === 2 ||
+                userChoice === 'rock' && compChoice === 1 ||
+                userChoice === 'scissors' && compChoice === 0) 
+    {
+        endGame("lose");
+    } else {
+        endGame("tie");
+    }
+}
+function endGame(outcome) {
+    gameData.game++;
+    gameDisplay.innerText = gameData.game;
+    gameData[outcome]++;
+    outcomeDisplay.innerText = `You ${outcome}`;
+    document.getElementById(outcome).innerText++;
 }
