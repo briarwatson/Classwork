288a52f8d0b0afd45a84938460551410c1be61fd
briarwatson <briar.watson402@gmail.com>
2023-09-27 14:47:10 -0500

    new updates

--- Week 5/ToDoListProject.js
+++ Week 5/ToDoListProject.js
@@ -17,3 +17,22 @@ function addLeft() {
     leftList.appendChild(newLi);
 
 }
+
+function addRight() {
+    const rightList = document.querySelector("ul.rightList");
+    const inputField = document.getElementById("rightInput");
+
+    const newLi = document.createElement("li");
+    const newInput = document.createElement("input");
+    newInput.type = "checkbox";
+
+    newLi.appendChild(newInput);
+    newLi.appendChild(document.createTextNode(inputField.value));
+    rightList.appendChild(newLi);
+}
+
+function addCheckBoxListner(checkbox) {
+    checkbox.addEventListener("click", () => {
+        this.parentElement.remove()
+    })
+}
