97e3d2b3ce0e0d1c442f7a62caef70b468804ff0
briarwatson <briar.watson402@gmail.com>
2023-10-09 12:15:16 -0500

    updates

---
+++ Calculator Project/Calculator.js
@@ -0,0 +1,12 @@
+const empty = document.getElementsByClassName("empty")[0];
+const box = document.getElementsByClassName("box");
+
+[...box].forEach(i => {
+    i.addEventListener("click", buttonClick);
+});
+
+function buttonClick(event) {
+    const value = event.target.innerText;
+
+    empty.innerText = value;
+}
