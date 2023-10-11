864cbdb2e2e28de8cbfa95ea744f820cfbce2c27
briarwatson <briar.watson402@gmail.com>
2023-10-11 18:02:12 -0500

    new updates

--- Calculator Project/Calculator.js
+++ Calculator Project/Calculator.js
@@ -9,11 +9,25 @@ function buttonClick(event) {
     const value = event.target.innerText;
 
     empty.innerText = value;
+
+    if (value === "AC") {
+        empty.innerText = 0;
+    } else
+
+    if (value === "+/-") {
+        let number = empty.innerText;
+        number *= -1;
+    } else
+
+    if (value === "%") {
+        let number = empty.innerText;
+        number *= 0.01;
+    }
+    if (value === "="){
+        let number = empty.innerText;
+        const output = eval(number);
+    } else
+
 }
 
-function buttonClick(clear) {
-    const clear = value.target.innerText;
-
-    empty.innerText = clear;
-}
 
